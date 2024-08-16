a542a133287ed0640d6f446dad8317c7c667b2f3
FlavioCastr00 <154934234+FlavioCastr00@users.noreply.github.com>
2024-08-16 12:20:53 -0300

    Commit de um exemplo básico em Java

---
+++ Java/O Básico em Java/Operações/Operações.java
@@ -0,0 +1,15 @@
+public class Operações
+{
+    public static void main(String[] args)
+    {
+        int a = -5 + 8 * 6;
+        int b = (55 + 9) % 9;
+        int c = 20 + -3 * 5 / 8;
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+        
+        System.out.println("Resultado da operação A = " + a +
+                           "\nResultado da operação B = " + b +
+                           "\nResultado da operação C = " + c +
+                           "\nResultado da operação D = " + d);
+    }
+}
